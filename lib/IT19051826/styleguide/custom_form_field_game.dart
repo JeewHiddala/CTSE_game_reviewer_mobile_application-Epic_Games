b6971636382e82961b9bb445f59d9b86619bd5e8
deshani0000 <it19051826@my.sliit.lk>
2022-04-07 01:10:58 +0530

    added games style guide

---
+++ lib/IT19051826/styleguide/custom_form_field_game.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class CustomFormFieldGame extends StatelessWidget {
+  const CustomFormFieldGame({
+    Key? key,
+    required TextEditingController controller,
+    required FocusNode focusNode,
+    required TextInputType keyboardType,
+    required TextInputAction inputAction,
+    required String label,
+    required String hint,
+    required String initialValue,
+    required Function(String value) validator,
+    this.isObscure = false,
+    this.isCapitalized = false,
+    this.maxLines = 1,
+    this.isLabelEnabled = true,
+  })  : _keyboardtype = keyboardType,
+        _inputAction = inputAction,
+        _label = label,
+        _hint = hint,
+        _initialValue = initialValue,
+        _validator = validator,
+        super(key: key);
+
+  final TextInputType _keyboardtype;
+  final TextInputAction _inputAction;
+  final String _label;
+  final String _hint;
+  final String _initialValue;
+  final bool isObscure;
+  final bool isCapitalized;
+  final int maxLines;
+  final bool isLabelEnabled;
+  final Function(String) _validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(color: Colors.black),
+      initialValue: _initialValue,
+      maxLines: maxLines,
+      keyboardType: _keyboardtype,
+      obscureText: isObscure,
+      textCapitalization:
+          isCapitalized ? TextCapitalization.words : TextCapitalization.none,
+      textInputAction: _inputAction,
+      cursorColor: Colors.yellow,
+      validator: (value) => _validator(value!),
+      decoration: InputDecoration(
+        labelText: isLabelEnabled ? _label : null,
+        labelStyle: TextStyle(color: Colors.yellowAccent),
+        hintText: _hint,
+        fillColor: Colors.white,
+        filled: true,
+        hintStyle: TextStyle(
+          color: Colors.grey,
+        ),
+        errorStyle: TextStyle(
+          color: Colors.red,
+          fontSize: 18,
+          fontWeight: FontWeight.normal,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.amberAccent,
+            width: 2,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.red,
+            width: 2,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.red,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
